20d43de9ae44d00c0672fdfaafd2b97f999256be
Jeffrey Shaw <jxjshaw@gmail.com>
2016-02-04 01:28:41 -0800

    s;ee[

--- _site/scripts/main.js
+++ _site/scripts/main.js
@@ -2,10 +2,10 @@ $(window).load(function()
 {
    // executes when complete page is fully loaded, including all frames, objects and images
    console.log("fadeddd");
-   alert("fadeddd");
+   //alert("fadeddd");
    //$("#header-row").css("opacity", "1.0");
    $("#project-row").css("opacity", "1.0");
 }); 
 
 
- 
+
