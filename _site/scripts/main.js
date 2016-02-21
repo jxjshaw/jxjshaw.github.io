c691ee954d829d7b7127cd9c876ccc2fda5d3342
Jeffrey Shaw <jxjshaw@gmail.com>
2016-02-21 06:02:36 -0800

    new loading screen

--- _site/scripts/main.js
+++ _site/scripts/main.js
@@ -1,11 +1,18 @@
+
+
+
 $(window).load(function() 
 {
    // executes when complete page is fully loaded, including all frames, objects and images
    console.log("fadeddd");
    //alert("fadeddd");
    //$("#header-row").css("opacity", "1.0");
+   $('.loading-screen').css("opacity", "0.0");
    $("#project-row").css("opacity", "0.9");
 });
+
+
+
 
 
 // $('.project-gif').hover(
